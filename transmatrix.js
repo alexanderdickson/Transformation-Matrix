ed4c91a3f130772daa4bb8f7d1de152001578c31
Alex Dickson <alex@alexanderdickson.com>
2013-09-08 00:00:20 +1000

    Added axis and 4 planes

--- transmatrix.js
+++ transmatrix.js
@@ -2,16 +2,16 @@
 
 var tm = {};
 
-window.t = tm;
-
 tm.identityMatrix = [1, 0, 0, 0, 1, 0, 0, 0, 1];
 tm.matrix = tm.identityMatrix;
-tm.vertices = [
+tm.initialVertices = [
 	[75, 75],
+	[100, 50],
 	[125, 75],
-	[125, 150],
-	[75, 150]
+	[125, 120],
+	[75, 120]
 ];
+tm.vertices = tm.initialVertices.slice();
 
 tm.inputs = null;
 tm.ctx = null;
@@ -27,25 +27,75 @@ tm.calculateMatrix = function() {
 				value = value.replace(/(sin|cos)\((\d(?:\.\d+)?)\)/, function(all, type, number) {
 						return Math[type](number);
 				});
+				// Replace PI with the real value.
+				value = value.replace(/\bPI\b/, function() {
+					return Math.PI;
+				});
 				return parseFloat(value) || 0; 
 			});
-	this.updateInputs();
-	this.vertices.forEach(function(vertex, i) {
-		vertex[0] = matrix[0] * vertex[0] + matrix[1] * vertex[1] + matrix[2];
-		vertex[1] = matrix[3] * vertex[0] + matrix[4] * vertex[1] + matrix[5];
+	this.vertices = this.vertices.map(function(vertex, i) {
+		return [matrix[0] * vertex[0] + matrix[1] * vertex[1] + matrix[2],
+		        matrix[3] * vertex[0] + matrix[4] * vertex[1] + matrix[5]];
 	});
 };
 
 tm.draw = function() {
 	var ctx = this.ctx;
+	var canvas = ctx.canvas;
+	var i;
+	var pixelGapBetweenPoints = 20;
+	var currentValue;
+	var midWidth = canvas.width / 2;
+	var midHeight = canvas.height / 2;
+ 	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+	ctx.strokeStyle = ctx.fillStyle = "#ccc";
+	ctx.beginPath();
+
+	// Draw X axis.
+	for (i = -midWidth; i < midWidth; i += pixelGapBetweenPoints) {
+		currentValue = Math.floor(i / pixelGapBetweenPoints);
+		if ( ! currentValue) {
+			continue;
+		}
+     	if ( ! (currentValue % 2)) {
+     		ctx.fillText(currentValue, i - 3, -6);
+     	}
+		ctx.moveTo(i + .5, -3);
+		ctx.lineTo(i + .5, 0);
+	}
+
+	ctx.moveTo(-midWidth, .5);
+	ctx.lineTo(midWidth, .5);
+
+	// Draw Y axis.
+	for (i = -midHeight; i < midHeight; i += pixelGapBetweenPoints) {
+		currentValue = Math.floor(i / pixelGapBetweenPoints);
+		if ( ! currentValue) {
+			continue;
+		}
+     	if ( ! (currentValue % 2)) {
+	     	ctx.fillText(currentValue, 3, i );
+     	}
+		ctx.moveTo(-3, i + .5);
+		ctx.lineTo(0, i + .5);
+	}
+
+	ctx.moveTo(-3.5, -midHeight);
+	ctx.lineTo(-3.5, midHeight);
+
+	ctx.closePath();
+	ctx.stroke();
+
+	// Draw object.
 	ctx.fillStyle = "goldenrod";
-	ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 	ctx.beginPath();
 	this.vertices.forEach(function(vertex) {
 		ctx.lineTo(vertex[0] + .5, vertex[1] + .5);
 	});
 	ctx.closePath();
 	ctx.fill();
+	ctx.stroke();
 };
  
 tm.updateInputs = function() {
@@ -59,6 +109,8 @@ tm.init = function() {
 	document.addEventListener("DOMContentLoaded", (function() {
 		var form = document.forms[0];
 		this.ctx = document.querySelector("canvas").getContext("2d");
+		this.ctx.translate(this.ctx.canvas.width / 2, this.ctx.canvas.height / 2);
+
 		this.draw();
 		this.inputs = [].slice.call(form.querySelectorAll("input[type='number']"));
 		this.updateInputs();
@@ -70,13 +122,13 @@ tm.init = function() {
 		form.addEventListener("reset", (function(event) {
 			event.preventDefault();
 			this.matrix = this.identityMatrix;
+			tm.vertices = tm.initialVertices.slice();
 			this.updateInputs();
 			this.calculateMatrix();
 			this.draw();
 		}).bind(this));
 		form.addEventListener("keyup", (function(event) {
 			var value = event.target.value;
-			console.log(value, event)
 			event.target.type = /^-?\d+(?:\.\d+)?$/.test(value) ? "number" : "text";
 		}).bind(this));
 	}).bind(this));
